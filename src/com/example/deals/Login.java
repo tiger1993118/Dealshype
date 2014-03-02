140100e7cd1608ac44e6ed401f22cd6fe9d4a275
Tiger <yi@yi.local>
2014-03-02 11:39:03 -0500

    Login with facebook function

--- src/com/example/deals/Login.java
+++ src/com/example/deals/Login.java
@@ -4,16 +4,19 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 
+import com.example.deals.sns.FacebookShare;
+
 public class Login extends Activity implements OnEditorActionListener {
 	EditText etEmail;
-
+	FacebookShare fbShare;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,6 +27,9 @@ public class Login extends Activity implements OnEditorActionListener {
 		etEmail.setTextColor(Color.parseColor("#FAFAFA"));
 		etEmail.setOnEditorActionListener(this);
 
+		fbShare = FacebookShare.getInstance();
+		fbShare.setActivity(this);
+		fbShare.onCreate(savedInstanceState);
 	}
 
 	@Override
@@ -31,5 +37,25 @@ public class Login extends Activity implements OnEditorActionListener {
 		Intent iTiles = new Intent("com.example.deals.Tiles");
 		startActivity(iTiles);
 		return false;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if(fbShare.isLoggedOn()){
+			Intent iTiles = new Intent("com.example.deals.Tiles");
+			startActivity(iTiles);
+			finish();
+		}
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		fbShare.onActivityResult(requestCode, resultCode, data);
+	}
+
+	public void signInFb(View view){
+		fbShare.requestLogIn();
 	}
 }
